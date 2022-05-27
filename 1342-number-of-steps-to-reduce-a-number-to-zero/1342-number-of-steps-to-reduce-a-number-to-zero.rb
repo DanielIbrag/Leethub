f7e9bd1afb151f9f857014baf0dfa8c7de4685ea
DanielIbrag <100488116+DanielIbrag@users.noreply.github.com>
2022-05-27 11:58:00 -0400

    Time: 123 ms (23.53%), Space: 211 MB (85.88%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.rb
@@ -0,0 +1,14 @@
+# @param {Integer} num
+# @return {Integer}
+def number_of_steps(num)
+    count = 0
+    while num > 0 
+    if num % 2 == 0
+        num = num/2
+    else
+        num = num-1
+    end
+        count+=1
+    end
+    return count
+end
